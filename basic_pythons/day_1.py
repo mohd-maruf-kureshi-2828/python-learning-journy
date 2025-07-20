2f6b6dad42790af9499ca0673dbe6c635edc6d30
Mohamed maruf kureshi <mohamedmarufkureshikureshi@gmail.com>
2025-07-20 12:53:44 +0000

    python file added

---
+++ basic_pythons/day_1.py
@@ -0,0 +1,37 @@
+# scripting python how write
+print("hello word")
+
+# procedural python how write
+# def firstDay():
+#     print("First Day.😎")
+# firstDay()
+
+#object-oriented programming language
+# class Demo:
+#     def firstDay(self):
+        # print("Just For Fun.🎉")
+
+# storeClass=Demo
+# Demo.firstDay()
+
+
+#practice for fun
+
+#scripting python we add two number
+num1=20
+num2=50
+# print(num1+num2)
+
+# procedural
+def sumNum(num1,num2):
+    print(num1+num2)
+# sumNum(num1,num2)
+
+
+#object oriented language
+class AddedNum:
+    def addTwoNum(num1,num2):
+        print(num1+num2)
+
+finalResult=AddedNum
+AddedNum.addTwoNum(num1,num2)
