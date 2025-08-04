992e2bb390af28b4851e8db7847c46ac8435428c
Mohamed maruf kureshi <mohamedmarufkureshikureshi@gmail.com>
2025-08-04 17:12:01 +0000

    Python File Added

---
+++ 04 function/02_Assigment.py
@@ -0,0 +1,11 @@
+def ourHospital(name, blood, *disease, emailId="@defualt.com"):
+    print(f"patient name {name},patient blood {blood} , patient disease {disease} ,patient emailId {emailId}")
+
+
+# ourHospital("arbaz", "B", "cold", emailId="@arbaz.com")
+
+
+def Python_Application(Std_name,std_Address,*Email):
+    print(f"This Studenet Name Is {Std_name} Emails Id IS {Email} Studend Address IS {std_Address}")
+
+Python_Application("Maruf Kureshi","Bangalore","Gmail.com")
