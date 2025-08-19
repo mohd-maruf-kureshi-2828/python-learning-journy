254399ce2524a4b77b77c498ece0a2e2b6c44215
Mohamed maruf kureshi <mohamedmarufkureshikureshi@gmail.com>
2025-08-19 05:45:44 +0000

    Python File Added

---
+++ Explore/Explore List Method/02_List.py
@@ -0,0 +1,10 @@
+# find negative number
+My_list=[-1,2,-3,-4,5,6,7,8]
+
+for i in My_list:
+    if i<0:
+        print(f"Negative Element : {i}")
+
+for i in My_list:
+    if i>=0:
+        print(f"Positive Element : {i}")
