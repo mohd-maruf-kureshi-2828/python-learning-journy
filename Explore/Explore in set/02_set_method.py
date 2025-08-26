6ce871e3c4e9779352e5b24daeb3bdf9598fb250
Mohamed maruf kureshi <mohamedmarufkureshikureshi@gmail.com>
2025-08-26 06:29:12 +0000

    Python File Added

---
+++ Explore/Explore in set/02_set_method.py
@@ -0,0 +1,17 @@
+# my_set={1,2,3,4}
+# my_set1={3,4,5,6}
+
+# print(my_set.union(my_set1)) union me reapted value count nhi hote
+# print(my_set.isdisjoint(my_set1)) isdisjoin method false diga q ke dono me 3 4 hai agar repeted value na hote tu true dita
+
+my_set={1,2,3}
+my_set1={1,2}
+# print(my_set.issubset(my_set1)) True diga jab pura set us set me bhi ho tu
+# print(my_set.issuperset(my_set1))
+# print(my_set.intersection(my_set1))
+# print(my_set.difference(my_set1))
+# my_set.remove(5)
+# print(my_set)
+
+# my_set.discard(5)
+# print(my_set)
